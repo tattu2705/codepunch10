41454f164937ade464556a1cbaa4c77596daf70c
tattu2705 <tattupro2705@gmail.com>
2023-06-07 19:13:35 +0700

    ok

--- back-end/dal/connectDB.php
+++ back-end/dal/connectDB.php
@@ -1,8 +1,8 @@
 <?php
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
+        $servername = "dbaas-db-4768168-do-user-14205969-0.b.db.ondigitalocean.com";
+        $username = "doadmin";
+        $password = "AVNS_OL_u8TrGI3MbcS-7vJJ";
         $dbname = "quanly"; 
         
         $conn = mysqli_connect($servername, $username, $password, $dbname);
